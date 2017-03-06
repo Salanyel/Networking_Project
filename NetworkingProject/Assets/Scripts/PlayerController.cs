ec8e97e2b0420f69b91310bcda2670075b1ddfd9
Xyunn <maxime.devecchi@gmail.com>
2017-03-06 13:46:59 +0100

    First push into the repository

---
+++ NetworkingProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float m_scaleHorizontal = 150.0f;
+    public float m_scaleVertical = 3.0f;
+
+    // Update is called once per frame
+    void Update () {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * m_scaleHorizontal;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * m_scaleVertical;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
