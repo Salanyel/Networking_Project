094bb26958ac58f99b52784b61aa921059b40a99
Xyunn <maxime.devecchi@gmail.com>
2017-03-06 14:02:56 +0100

    Adding network displacement

--- NetworkingProject/Assets/Scripts/PlayerController.cs
+++ NetworkingProject/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 
     public float m_scaleHorizontal = 150.0f;
     public float m_scaleVertical = 3.0f;
 
     // Update is called once per frame
     void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+
+        }
+
+        detectInput();
+
+    }
+
+    /// <summary>
+    /// Detects the input and displace the player
+    /// </summary>
+    void detectInput()
+    {
         float x = Input.GetAxis("Horizontal") * Time.deltaTime * m_scaleHorizontal;
         float z = Input.GetAxis("Vertical") * Time.deltaTime * m_scaleVertical;
 
