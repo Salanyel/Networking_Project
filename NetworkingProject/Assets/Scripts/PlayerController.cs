b08678751808eeadd2bbe1230d566f8c12a446bb
Maxime de VECCHI <maxime.devecchi@gmail.com>
2017-03-07 13:04:08 +0100

    Shooting single
    Adding the ability for the player to shoot (#4)

--- NetworkingProject/Assets/Scripts/PlayerController.cs
+++ NetworkingProject/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,14 @@ public class PlayerController : NetworkBehaviour {
     public float m_scaleHorizontal = 150.0f;
     public float m_scaleVertical = 3.0f;
 
+    /// <summary>
+    /// Fields for the bullet
+    /// </summary>
+    public GameObject m_bulletPrefab;
+    public Transform m_bulletSpawn;
+    public float m_bulletSpeed = 6.0f;
+    public float m_bulletLifeTime = 2.0f;
+
     // Update is called once per frame
     void Update () {
         if (!isLocalPlayer)
@@ -29,6 +37,23 @@ public class PlayerController : NetworkBehaviour {
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fire();
+        }
+    }
+
+    void fire()
+    {
+        //Create the Bullet from the bullet prefab
+        GameObject bullet = Instantiate(m_bulletPrefab, m_bulletSpawn.position, m_bulletSpawn.rotation) as GameObject;
+
+        //Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * m_bulletSpeed;
+
+        //Destory the bullet after a certain time
+        Destroy(bullet, m_bulletLifeTime);
     }
 
     /// <summary>
