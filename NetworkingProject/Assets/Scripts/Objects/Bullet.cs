471c88eb5609c9dc9d885efe8007c2b218d5f70b
Maxime de VECCHI <maxime.devecchi@gmail.com>
2017-03-07 13:34:14 +0100

    12_Player_Health
    Adding the bullet damage, the player health and the display of the health (#6)

---
+++ NetworkingProject/Assets/Scripts/Objects/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public int m_bulletDamage = 10;
+
+    /// <summary>
+    /// Destory the current bullet when colliding with an other object
+    /// </summary>
+    /// <param name="collision">Collision: other objects collided</param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        Health health = collision.gameObject.GetComponent<Health>();
+
+        if (health != null)
+        {
+            health.takeDamage(m_bulletDamage);
+        }
+
+        Destroy(gameObject);
+    }
+
+}
